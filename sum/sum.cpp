014d68452870194ad1ecd12c9dc94f92e0d9bc94
Ульяна Митюшёва <iu@MacBook-Ulana.local>
2021-02-22 17:07:15 +0300

    Add sum.cpp, sum.h

---
+++ sum/sum.cpp
@@ -0,0 +1,4 @@
+#include "sum.h"
+int64_t Sum(int32_t x, int32_t y) {
+    return x + y;
+}
---
+++ sum/sum.h
@@ -0,0 +1,8 @@
+#ifndef SUM_SUM_H
+#define SUM_SUM_H
+
+#include <cstdint>
+
+int64_t Sum(int32_t x, int32_t y);
+
+#endif  // SUM_SUM_H
