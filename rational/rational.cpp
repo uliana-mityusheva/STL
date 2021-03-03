3ca17e5000c607a28ec6e7d86804c6786e9f6c95
Ульяна Митюшёва <iu@MacBook-Ulana.local>
2021-03-03 10:42:27 +0300

    Error correction

--- rational/rational.cpp
+++ rational/rational.cpp
@@ -70,7 +70,7 @@ void Rational::SetDenominator(int num) {
     Reduce();
 }
 
-friend Rational::std::istream &operator>>(std::istream &is, Rational &num) {
+std::istream &operator>>(std::istream &is, Rational &num) {
     const int MaxStringSize = 32;
     char str[MaxStringSize];
     is >> str;
@@ -86,7 +86,7 @@ friend Rational::std::istream &operator>>(std::istream &is, Rational &num) {
     return is;
 }
 
-friend Rational::std::ostream &operator<<(std::ostream &out, Rational &num) {
+std::ostream &operator<<(std::ostream &out, Rational &num) {
     if (num.denominator != 1) {
         out << num.numerator << "/" << num.denominator;
     } else {
